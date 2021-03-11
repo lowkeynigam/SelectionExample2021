4d15db5f19fc8f80a13741b0f17fe44ca73100dd
Rishabh Nigam <fire2407doesminecraft@gmail.com>
2021-03-11 17:02:27 +1100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SelectionExample2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
